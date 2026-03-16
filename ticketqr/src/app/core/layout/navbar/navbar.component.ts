7371d2a762f77bf35127059fa6c3a68bb8b3c196
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2026-03-16 00:09:00 +0000

    Features : Unique qrcode handling

--- ticketqr/src/app/core/layout/navbar/navbar.component.ts
+++ ticketqr/src/app/core/layout/navbar/navbar.component.ts
@@ -6,14 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  menuOpen = false;
-
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
-
-  closeMenu() {
-    this.menuOpen = false;
-  }
-}
+export class NavbarComponent {}
